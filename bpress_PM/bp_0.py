70232e90f58d6b00dfc80ce46aeff0278efb7932
Erik Fredrickson <erikfred@uw.edu>
2022-06-22 12:00:15 -0700

    small commented notes to bpress_PM codes

--- bpress_PM/bp_0.py
+++ bpress_PM/bp_0.py
@@ -14,7 +14,7 @@ from lo_tools import plotting_functions as pfun
 # set mooring extraction to analyze (just needs salt, temp, and zeta)
 Ldir = Lfun.Lstart()
 
-out_dir = Ldir['parent'] / 'LPM_output' / 'bpress'
+out_dir = Ldir['parent'] / 'LO_output' / 'bpress_PM'
 Lfun.make_dir(out_dir)
 
 #sn_list = ['CE01', 'CE02', 'CE04', 'PN01A']
