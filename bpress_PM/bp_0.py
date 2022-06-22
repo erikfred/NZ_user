43cdbe6e1019a3d5dda744e88a6c0d11c3480952
Erik Fredrickson <erikfred@uw.edu>
2022-06-22 12:01:08 -0700

    small commented notes to bpress_PM codes

--- bpress_PM/bp_0.py
+++ bpress_PM/bp_0.py
@@ -14,13 +14,13 @@ from lo_tools import plotting_functions as pfun
 # set mooring extraction to analyze (just needs salt, temp, and zeta)
 Ldir = Lfun.Lstart()
 
-out_dir = Ldir['parent'] / 'LO_output' / 'bpress_PM'
+out_dir = Ldir['parent'] / 'LO_output' / 'bpress_PM' # I think this assumes I'm working out of the bpress subfolder
 Lfun.make_dir(out_dir)
 
-#sn_list = ['CE01', 'CE02', 'CE04', 'PN01A']
+# sn_list = ['CE01', 'CE02', 'CE04', 'PN01A'] # OOI cabled stations?
 sn_list = ['PN01A']
 
-fs = 12
+fs = 12 # fontsize
 def icb(ax, cs):
     # cbaxes = inset_axes(ax, width="40%", height="4%", loc='upper right', borderpad=2)
     # cb = fig.colorbar(cs, cax=cbaxes, orientation='horizontal')
@@ -33,6 +33,7 @@ plt.close('all')
 for sn in sn_list:
 
     fn = Ldir['LOo'] / 'extract' / 'cas6_v3_lo8b' / 'moor' / 'ooi' / (sn + '_2018.01.01_2018.12.31.nc')
+    # ‘/data1/parker/LO_roms/cas6_v0_live’
     ds = xr.open_dataset(fn)
 
     # get time axis
